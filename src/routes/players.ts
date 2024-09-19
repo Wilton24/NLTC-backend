199a0a0b830d9d14787eaff991ffca9f8fcaa1c0
Wilton24 <cbasher24@gmail.com>
2024-09-19 18:43:37 +0800

    fixed, file input system

--- src/routes/players.ts
+++ src/routes/players.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 // Player routes 
 router.get('/players', getPlayers);
 router.get('/players/:id', getOnePlayer);
-router.post('/players', handleProfilePicUpload, createPlayer);
+router.post('/players',handleProfilePicUpload, createPlayer);
 router.delete('/players/:id', deletePlayer);
 
 router.get('/sample', async(req: Request, res: Response)=>{
